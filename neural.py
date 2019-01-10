0b18eb79e9647cc1d3123f05fb2070410b480ed7
Jack Scantlebury <jack.scantlebury@gmail.com>
2019-01-10 15:31:31 +0000

    Commented functions and methods

--- neural.py
+++ neural.py
@@ -10,6 +10,10 @@ import time
 
 
 class Timer:
+    """
+    For timing things. Destructor records lifetime of object, stored in
+    interval
+    """
     def __enter__(self):
         self.start = time.time()
         return self
@@ -22,21 +26,36 @@ class Timer:
         self.interval = end - self.start
         
 def rot180(x):
+    """
+    Rotates matrix by 180 degrees
+    """
     return np.rot90(np.rot90(x))
 
 def is_square(x):
+    """
+    Checks whether matrix is square
+    """
     try:
         return x.shape[0] == x.shape[1]
     except Exception:
         return False
 
 def make_vec(x):
+    """
+    Flattens matrix into column vector
+    """
     return x.reshape((-1, ))
 
-def xavier(dims):
-    return np.random.randn(dims[0], dims[1])/np.sqrt(dims[0]/2)
+def xavier(*dims):
+    """
+    Returns a tensor, matrix or vector initialised by Xavier initialisation
+    """
+    return np.random.randn(*dims)/np.sqrt(dims[0]/2)
 
 def normalise(x):
+    """
+    Normalise vectors or matrices
+    """
     try:
         if len(x.shape) == 1 or x.shape[1] == 1:
             x -= np.mean(x)
@@ -55,46 +74,79 @@ def normalise(x):
     return res.reshape(x.shape)
 
 def ReLU(x):
+    """
+    Rectivied linear unit function (nonlinearity)
+    """
     res = x
     res[np.where(res < 0)] *= 0.01
     return res
 
 def ReLU_prime(x):
+    """
+    First differential of ReLU
+    """
     res = np.ones_like(x)
     res[np.where(x < 0)] = 0.01
     return res
 
+# This is inelegant. Redo/scrap.
 def make_square(x):
+    """
+    Attempt to make vector or matrix into square matrix.
+    """
     if not is_square(x):
         try:
             return x.reshape(int(np.sqrt(x.shape[0]*x.shape[1])), -1)
         except Exception:
             return x.reshape(int(np.sqrt(x.shape[0])), -1)
 
+# For not adding non-linearity between layers
 def identity(x):
+    """
+    No nonlinearlity
+    """
     return x
 
 def identity_prime(x):
+    """
+    First differential of identity (just ones)
+    """
     return np.ones_like(x)
 
 def cross_entropy_loss(label_vec, predicted_vec):
+    """
+    Cross entropy loss function:
+        Negative sum of the labels multiplied by the log of their estimated label
+        probability
+    """
     loss = 0
     for i in range(len(label_vec)):
         loss -= label_vec[i]*np.log2(predicted_vec[i])
     return loss
 
 def sq_err(label_vec, predicted_vec):
-    return np.sum(np.multiply(label_vec-predicted_vec, label_vec-predicted_vec))
+    """
+    Loss function (square error)
+    """
+    return np.sum(np.multiply(label_vec-predicted_vec,
+                              label_vec-predicted_vec))
 
 def softmax(activations):
+    """
+    Softmax nonlinear function for use in final layer
+    """
     ex = np.exp(activations)
     denominator = np.sum(ex)
     return ex/denominator
 
 class fully_connected_layer:
+    """
+    Fully connected layer for use in neural net object; all neurons in layer
+    l-1 are connected to all neurons in layer l.
+    """
     
     def __init__(self, dims, a_fn, a_prime_fn, final=False):
-        self.w = xavier(dims)
+        self.w = xavier(dims[0], dims[1])
         self.b = np.random.randn(self.w.shape[0])/np.sqrt(dims[1]/2)
         self.z = np.zeros((self.w.shape[0], ))
         self.a = np.zeros((self.w.shape[0], ))
@@ -112,21 +164,34 @@ class fully_connected_layer:
         
         
     def forward(self, inputs):
+        """
+        Forward propagation a = sigma(w'x + b)
+        """
         self.z = self.w @ make_vec(inputs) + self.b
         self.a = self.sigma(self.z)
         
     def backward(self, delta=None, output=None, labels=None, w=None):
-        if self.final:
+        """
+        Propagates error signal dE/dz back through layer
+        """
+        if self.final: # Only for use with softmax (usual case)
             self.delta = output - labels
-        else:
+        else: # Can use any sigma for other layers
             self.delta = np.multiply(w.T @ delta,
                                      self.sigma_prime(self.z))
     
     def update_grads(self, inputs):
+        """
+        Update knowledge about local gradients
+        """
         self.grad_w += np.outer(self.delta, inputs.reshape(-1,))
         self.grad_b += self.delta
         
     def apply_grads(self, lr, bs, rho=0):
+        """
+        Modify layer gradients using cached local gradient information then
+        reset cache
+        """
         self.vw = rho * self.vw + self.grad_w*lr/bs
         self.vb = rho * self.vb + self.grad_b*lr/bs
         self.w -= self.vw
@@ -193,7 +258,10 @@ class convolutional_layer:
         
 
 class neural_net:
-    
+    """
+    Requires fully connected or convolutional (unfinished) layers specified 
+    in the above classes
+    """
     def __init__(self, train_x, train_y, test_x, test_y, 
                  init_type='random', losstype='acc'):
         
@@ -211,6 +279,9 @@ class neural_net:
         self.test_acc = []
         
     def add_layer(self, layer):
+        """
+        layer should be instance of fully_connected_layer
+        """
         if len(self.layers) > 0:
             self.layers[-1].final = False
             self.layers[-1].normalfn = normalise
@@ -218,31 +289,50 @@ class neural_net:
         self.layers[-1].final = True
         self.layers[-1].normalfn = identity
     
-    def forward_prop(self, x):       
+    def forward_prop(self, x):  
+        """
+        Performs forwards propagation across all layers (whole architecture)
+        """
         self.layers[0].forward(x)
         for idx in range(1, len(self.layers)):
             self.layers[idx].forward(self.layers[idx-1].a)
         self.output_layer = self.layers[-1].a
     
     def back_prop(self, x, labels):
+        """
+        Propagates error signal dE/dz backwards across all layers
+        """
         self.layers[-1].backward(delta=None, output=self.output_layer,
                                  labels=labels)
         for idx in range(len(self.layers) - 2, -1, -1):
             self.layers[idx].backward(delta=self.layers[idx+1].delta,
                                       w=self.layers[idx+1].w)
             
-        
             
     def update_grads(self, x):
+        """
+        Adds knowledge of all gradients to cache across all layers
+        """
         self.layers[0].update_grads(x)
         for idx in range(1, len(self.layers)):
             self.layers[idx].update_grads(self.layers[idx-1].a)
             
     def apply_grads(self, lr, bs):
+        """
+        Applies cached gradient information to all gradients across all layers
+        """
         for layer in self.layers:
             layer.apply_grads(lr, bs, rho=0.9)
                         
-    def train(self, epochs, batch_size, lr):
+    def train(self, epochs, batch_size, lr=1e-3):
+        """
+        Trains neural network
+        Parameters:
+            epochs: Number of times training data should be used
+            batch_size: number of inputs used in update_grads before using
+                apply_grads
+            lr: learning rate (should not be above 1e-3)
+        """
         for epoch in range(epochs):
             samples = np.random.choice(self.train_x.shape[0],
                                        self.train_x.shape[0],
@@ -296,6 +386,12 @@ class neural_net:
             "\rTraining: 100%, test accuracy: " + test_acc + "\n")
             
     def test(self, n, mode):
+        """
+        Tests model accuracy on random subsample of test data of size n.
+        Paramters:
+            n: sample size
+            mode: 'acc': Ratio of correct/total classifications \in [0, 1]
+        """
         wrong = 0
         samples = np.random.choice(self.test_x.shape[0], n, replace=False)
         loss = 0
@@ -321,6 +417,9 @@ class neural_net:
             return loss/n
     
     def classify(self, imgpath=None, img=None):
+        """
+        Given an image, output the classification given by the (trained) NN
+        """
         try:
             img.shape
         except Exception:
@@ -330,7 +429,11 @@ class neural_net:
         return np.argmax(self.output_layer)
                 
     @property
-    def info(self):
+    def _info(self):
+        """
+        (Debugging tool)
+        Prints layer dimension info
+        """
         print("\nActivations:")
         for layer in self.layers:
             print(layer.name, layer.a.shape)
@@ -349,7 +452,11 @@ class neural_net:
             
     
     @property
-    def maxmins(self):
+    def _maxmins(self):
+        """
+        (Debugging tool)
+        Prints the max/min of each layer of weights
+        """
         print("\nActivations:")
         for layer in self.layers:
             print(np.amin(layer.a), np.amax(layer.a))
@@ -367,32 +474,27 @@ class neural_net:
             print(np.amin(layer.delta), np.amax(layer.delta))
             
     def plot(self):
+        """
+        Plots a graph of accuracy/loss as a function of seen data
+        """
         plt.plot(self.processed_arr, self.train_acc, 'r-')
         plt.plot(self.processed_arr, self.test_acc, 'b-')
         if self.losstype == 'acc':
             plt.ylim([0, 100])
         else:
             plt.ylim([0, 1.5])
-        ylab = 'Accuracy (%)' if self.losstype=='acc' else 'Cross entropy loss ' + r'(-$\Sigma_i p_i \ln{q_i}$)'
+        ylab = 'Accuracy (%)' if self.losstype=='acc' else 'Cross entropy loss ' + r'(-$\Sigma_i p_i \log_2 q_i$)'
         plt.ylabel(ylab)
         plt.xlabel('Data processed')
         title = 'Test and training accuracy on MNIST' if self.losstype == 'acc' else 'Test and training loss on MNIST'
         plt.title(title)
         plt.legend({'Training', 'Testing'})
-            
-    def plot_update(self):
-        self.h1.set_xdata(self.processed_arr)
-        self.h2.set_xdata(self.processed_arr)
-        self.h1.set_ydata(self.train_acc)
-        self.h2.set_ydata(self.test_acc)
-        plt.show()
-        try:
-            plt.gcf().canvas.flush_events()
-        except NotImplementedError:
-            pass
 
 def get_data():
-  return input_data.read_data_sets("MNIST_data/", one_hot=True)
+    """
+    This is the only placewhere we use tensorflow!
+    """
+    return input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 mnist = get_data()
 mnist_eval = get_data()
